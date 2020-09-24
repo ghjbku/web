b35f45fe798dafdfe2be15fe7ef64a751d7c9c7d
Balázs Tóth <gawnoris@gmail.com>
2020-09-24 10:38:27 +0200

    Update shoppin_cart.js

--- shoppin_cart.js
+++ shoppin_cart.js
@@ -46,9 +46,6 @@ function find_unit(val,index){
     if (val.price_per_unit > max){
     max = val.price_per_unit;
     }
-    if (index === shopping_cart.length-1){
-    console.log("maximum/unit price: "+val.product+" "+max);
-    }
 }
 let most_total = 0;
 function find_most_total(val,index){
@@ -57,9 +54,6 @@ function find_most_total(val,index){
 
     if (helper > most_total){
             most_total = helper;
-            if (index === shopping_cart.length-1){
-                console.log("max of total:"+val.product+" "+most_total);
-                }
     }
 }
 shopping_cart.forEach(find_unit);
